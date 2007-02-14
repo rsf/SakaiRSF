831b1e9d57d50ccaa5fe9d1aaa62d8183145e52e
Steven Githens <swgithen@mtu.edu>
2007-02-14 20:14:09 +0000

    This adds support for ARI2 to HHHB, and partial support for UIELBindings on the Post Helper Method Binding

--- src/java/uk/ac/cam/caret/sakai/rsf/helper/HelperHandlerHookBean.java
+++ src/java/uk/ac/cam/caret/sakai/rsf/helper/HelperHandlerHookBean.java
@@ -14,9 +14,14 @@ import org.sakaiproject.tool.api.ToolException;
 import uk.org.ponder.beanutil.BeanLocator;
 import uk.org.ponder.beanutil.BeanModelAlterer;
 import uk.org.ponder.rsf.components.ELReference;
+import uk.org.ponder.rsf.components.ParameterList;
 import uk.org.ponder.rsf.components.UICommand;
+import uk.org.ponder.rsf.components.UIELBinding;
 import uk.org.ponder.rsf.components.UIOutput;
+import uk.org.ponder.rsf.components.UIParameter;
+import uk.org.ponder.rsf.flow.ARIProcessor;
 import uk.org.ponder.rsf.flow.ARIResult;
+import uk.org.ponder.rsf.flow.ActionResultInterceptor;
 import uk.org.ponder.rsf.flow.ActionResultInterpreter;
 import uk.org.ponder.rsf.preservation.StatePreservationManager;
 import uk.org.ponder.rsf.state.TokenStateHolder;
@@ -37,6 +42,8 @@ import uk.org.ponder.util.UniversalRuntimeException;
 public class HelperHandlerHookBean {
 
   private static final String TOKEN_STATE_PREFIX = "HelperHandlerHook";
+  private static final String POST_HELPER_BINDING_PARAMLIST = HelperViewParameters.POST_HELPER_BINDING + "PARAMLIST";
+  private static final String POST_HELPER_ARI2_VIEWPARAMS = HelperViewParameters.POST_HELPER_BINDING + "ARI2RETPARAMS";
   private static final String HELPER_FINISHED_PATH = "/done";
   private static final String IN_HELPER_PATH = "/tool";
 
@@ -50,6 +57,7 @@ public class HelperHandlerHookBean {
   private BeanModelAlterer bma;
   private BeanLocator beanLocator;
   private ActionResultInterpreter ari;
+  private ARIProcessor ariprocessor;
   private ActiveToolManager activeToolManager;
   private BaseURLProvider bup;
   private String pathInfo;
@@ -82,13 +90,28 @@ public class HelperHandlerHookBean {
 
     ELReference elref = (ELReference) tsh.getTokenState(TOKEN_STATE_PREFIX
         + viewParameters.viewID + HelperViewParameters.POST_HELPER_BINDING);
+    ParameterList paramlist = (ParameterList) tsh.getTokenState(TOKEN_STATE_PREFIX
+        + viewParameters.viewID + POST_HELPER_BINDING_PARAMLIST);
     statePreservationManager.scopeRestore();
+    if (paramlist != null) {
+      for (int i = 0; i < paramlist.size(); i++) {
+        UIParameter param = (UIParameter) paramlist.get(i);
+        if (param instanceof UIELBinding) {
+          bma.setBeanValue(((UIELBinding)param).valuebinding.value, beanLocator, 
+              ((UIELBinding)param).rvalue, null, false);
+        }
+      }
+    }
     String methodBinding = elref.value;
     Object beanReturn = methodBinding == null ? null
         : bma.invokeBeanMethod(methodBinding, beanLocator);
+    
+    ViewParameters originParams = (ViewParameters) tsh.getTokenState(TOKEN_STATE_PREFIX
+        + viewParameters.viewID + POST_HELPER_ARI2_VIEWPARAMS);
+        
 
     ARIResult ariresult = ari.interpretActionResult(viewParameters, beanReturn);
-
+    ariprocessor.interceptActionResult(ariresult, originParams, ariresult);
     String urlToRedirectTo = ViewParamUtil.getAnyFullURL(ariresult.resultingView, vsh);
     try {
       response.sendRedirect(urlToRedirectTo);
@@ -158,7 +181,17 @@ public class HelperHandlerHookBean {
       tsh.putTokenState(TOKEN_STATE_PREFIX + viewParameters.viewID
           + HelperViewParameters.POST_HELPER_BINDING,
           helperBinding.methodbinding);
+      //Support for a ParameterList on the UICommand
+      if (helperBinding.parameters != null) {
+        tsh.putTokenState(TOKEN_STATE_PREFIX + viewParameters.viewID 
+            + POST_HELPER_BINDING_PARAMLIST,
+            helperBinding.parameters);
+      }
     }
+    // We need to save these ViewParameters for ActionResultInterceptors
+    tsh.putTokenState(TOKEN_STATE_PREFIX + viewParameters.viewID 
+        + POST_HELPER_ARI2_VIEWPARAMS,
+        viewParameters);
 
     String helperToolPath = bup.getBaseURL() + viewParameters.viewID
         + IN_HELPER_PATH;
@@ -228,5 +261,9 @@ public class HelperHandlerHookBean {
 
   public void setPathInfo(String pathInfo) {
     this.pathInfo = pathInfo;
+  }
+  
+  public void setAriProcessor(ARIProcessor ari2p) {
+    this.ariprocessor = ari2p;
   }
 }
